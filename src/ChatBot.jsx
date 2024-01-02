015737935a126a3ea84e229bca84918afebabd52
QudsiaAhmed <QudsiaAhmed>
2024-01-02 13:06:37 +0500

    Add scroll bar and loader

--- src/ChatBot.jsx
+++ src/ChatBot.jsx
@@ -1,15 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useParams } from 'react-router-dom';
-import Typewriter from 'typewriter-effect';
 import { TypeAnimation } from "react-type-animation";
-// import SendIcon from '@mui/icons-material/Send';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
 
 const ChatBot = () => {
     const { id } = useParams();
-    console.log("id", id)
 
     const [loading, setLoading] = useState(true);
-    const [chatbotname, setchatbotname] = useState("");
+    const [chatbotname, setChatbotName] = useState("");
     const [styles, setStyles] = useState({
         bgColor: '',
         chatBubbleColor: '',
@@ -23,8 +22,6 @@ const ChatBot = () => {
         tagline: '',
         typingSpeed: '',
         userChatBubbleColor: 'lightblue',
-
-
     });
 
     const [messages, setMessages] = useState([
@@ -34,22 +31,21 @@ const ChatBot = () => {
     const [userInput, setUserInput] = useState('');
     const [user_id, setUserId] = useState('');
     const [role, setRole] = useState('');
+    const [loadingBotResponse, setLoadingBotResponse] = useState(false);
+
+    const messagesContainerRef = useRef(null);
+
     useEffect(() => {
         const fetchData = async () => {
             try {
                 const response = await fetch(`http://162.244.82.128:4003/chatbots/get-single-chatbot/${id}`);
                 const data = await response.json();
-                console.log('data', data);
                 const { chatbotStyles, chatbotDetails } = data?.data || {};
-                console.log("chatbotDetails", chatbotDetails);
                 const initialMessage = chatbotDetails?.initialMessage;
                 const name = chatbotDetails?.name;
-                setchatbotname(name)
-                console.log(name)
+                setChatbotName(name);
                 const chatbotRole = chatbotDetails?.role;
-                console.log('chatbotRole', chatbotRole);
                 const { userId } = data?.data;
-                console.log(userId);
                 setUserId(userId);
                 setRole(chatbotRole);
                 setMessages([{ content: initialMessage, sender: 'bot' }]);
@@ -75,15 +71,22 @@ const ChatBot = () => {
         };
 
         fetchData();
-    }, []);
+    }, [id]);
+
+    useEffect(() => {
+        if (messagesContainerRef.current) {
+            messagesContainerRef.current.scrollTop = messagesContainerRef.current.scrollHeight;
+        }
+    }, [messages]);
 
     const sendMessageToAPI = async (userMessage) => {
         const formdata = new FormData();
         formdata.append("query", userMessage);
-        formdata.append("chatbot_id", "6589678c4a3d102039b3fb4f");
+        formdata.append("chatbot_id", "6592837258dce0bb4d220e9f");
         formdata.append("verticals", role);
         formdata.append("user_id", 'shahzain');
-        console.log('formdata', formdata)
+        formdata.append("user_chatid",'Qudsia');
+
         try {
             const response = await fetch('http://162.244.82.128:8005/chat', {
                 method: 'POST',
@@ -91,10 +94,7 @@ const ChatBot = () => {
                     'authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoic2hhaHphaW4ifQ.XDPGU3IGuye85yaoptxIgd3PvjLQ2bGK0WSTto8VO7Y',
                 },
                 body: formdata,
-
                 redirect: 'follow',
-                // mode: 'no-cors',  // Set the mode to 'no-cors'
-
             });
 
             const result = await response.json();
@@ -115,43 +115,29 @@ const ChatBot = () => {
     const handleUserInput = (e) => {
         setUserInput(e.target.value);
     };
-    const handleSendMessage = async () => {
-        console.log('Sending message:', userInput);
 
+    const handleSendMessage = async () => {
         if (userInput.trim() !== '') {
+            setUserInput('');
+
             const userMessage = userInput;
 
-            // Add the user's message to the messages array
             addMessage(userMessage, 'user');
+            setLoadingBotResponse(true);
 
             try {
                 const apiResponse = await sendMessageToAPI(userMessage);
-                console.log('API Response:', apiResponse);
-
-                // Extract the message from the API response
                 const botResponse = apiResponse.Message || 'Sorry, something went wrong.';
-
-                // Add the bot's response to the messages array
                 addMessage(botResponse, 'bot');
-
             } catch (error) {
                 console.error('Error handling API response:', error);
                 addMessage('Sorry, something went wrong.', 'bot');
+            } finally {
+                setLoadingBotResponse(false);
             }
-
-            setUserInput('');
         }
     };
 
-
-    useEffect(() => {
-        if (messages[messages.length - 1]?.sender === 'user') {
-            // setTimeout(() => {
-            //     // const botResponse = 'I received your message! This is a bot response.';
-            //     addMessage(botResponse, 'bot');
-            // }, styles.typingSpeed);
-        }
-    }, [messages, styles.typingSpeed]);
     return (
         <div
             style={{
@@ -177,12 +163,11 @@ const ChatBot = () => {
                             background: `linear-gradient(to right, ${styles.headerGradientOne}, ${styles.headerGradientTwo})`,
                             padding: '10px',
                             textAlign: 'center',
-                            height: '30px', // Set the height of the header
-                            width: '100%', // Set the width of the header
+                            height: '30px',
+                            width: '100%',
                             display: 'flex',
                             alignItems: 'center',
                             justifyContent: 'center',
-
                         }}
                     >
                         {styles.icon && (
@@ -192,8 +177,6 @@ const ChatBot = () => {
                                 style={{ width: '24px', height: '24px', marginRight: '10px' }}
                             />
                         )}
-
-                        {/* <div style={{ fontSize: '24px', marginBottom: '5px' }}>{styles.icon}</div> */}
                         <div style={{ fontSize: '24px', padding: '5px 0', color: 'white', textAlign: 'center', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>{chatbotname}</div>
                         <div style={{ fontSize: '18px' }}>{styles.tagline}</div>
                     </div>
@@ -204,21 +187,36 @@ const ChatBot = () => {
                             overflowY: 'auto',
                             padding: '15px',
                             borderBottom: `1px solid ${styles.userChatBubbleColor}`,
-                            scrollbarWidth: 'thin', // Firefox
-                            scrollbarColor: `${styles.headerGradientOne} ${styles.headerGradientTwo}`, // Firefox
+                            scrollbarWidth: 'thin',
+                            scrollbarColor: `${styles.headerGradientOne} ${styles.headerGradientTwo}`,
                         }}
+                        ref={messagesContainerRef}
                     >
                         {messages.map((message, index) => (
                             <div
                                 key={index}
                                 style={{
-                                    textAlign: message.sender === 'user' ? 'right' : 'left',
+                                    display: 'flex',
+                                    flexDirection: message.sender === 'user' ? 'row-reverse' : 'row',
+                                    alignItems: 'flex-start',
                                     marginBottom: '10px',
                                 }}
                             >
+                                {message.sender === 'bot' && styles.icon && (
+                                    <img
+                                        src={styles.icon}
+                                        alt="Bot Icon"
+                                        style={{
+                                            width: '40px',
+                                            height: '40px',
+                                            marginRight: '10px',
+                                            alignSelf: 'flex-end',
+                                        }}
+                                    />
+                                )}
+
                                 <div
                                     style={{
-                                        display: 'inline-block',
                                         background:
                                             message.sender === 'user'
                                                 ? styles.userChatBubbleColor
@@ -241,7 +239,11 @@ const ChatBot = () => {
                                 </div>
                             </div>
                         ))}
-
+                        {loadingBotResponse && (
+                            <div style={{ textAlign: 'center', marginTop: '10px' }}>
+                                <p>Loading...</p>
+                            </div>
+                        )}
                     </div>
 
                     <div
@@ -263,7 +265,7 @@ const ChatBot = () => {
                                 outline: 'none',
                                 backgroundColor: 'white',
                                 color: 'black',
-                                marginRight: '10px', // Add margin for space
+                                marginRight: '10px',
                             }}
                             value={userInput}
                             onChange={handleUserInput}
@@ -284,7 +286,8 @@ const ChatBot = () => {
                             }}
                             onClick={handleSendMessage}
                         >
-                            {/* <SendIcon style={{ marginRight: '5px' }} /> */}
+    <FontAwesomeIcon icon={faPaperPlane} style={{ marginRight: '5px' }} />
+
                         </button>
                     </div>
 
@@ -296,12 +299,11 @@ const ChatBot = () => {
                             textAlign: 'center',
                             borderTop: `1px solid ${styles.userChatBubbleColor}`,
                             padding: '10px',
-                            height: 'auto', // Set the height of the footer
-                            width: '100%', // Set the width of the footer
+                            height: 'auto',
+                            width: '100%',
                         }}
                     >
                         <div style={{ fontSize: '18px', marginTop: '5px', color: 'black' }}>Building an Unbiased World</div>
-                        {/* <div style={{ fontSize: '24px', marginBottom: '5px' }}>{styles.icon}</div> */}
                         <div style={{ fontSize: '18px' }}>{styles.tagline}</div>
                     </div>
 
@@ -311,8 +313,8 @@ const ChatBot = () => {
                             padding: '10px',
                             textAlign: 'center',
                             borderTop: `1px solid ${styles.userChatBubbleColor}`,
-                            height: '30px', // Set the height of the footer
-                            width: '100%', // Set the width of the footer
+                            height: '30px',
+                            width: '100%',
                         }}
                     >
                         <div style={{
@@ -322,7 +324,6 @@ const ChatBot = () => {
                         }}></div>
                         <div style={{ fontSize: '18px' }}>{styles.tagline}</div>
                     </div>
-
                 </>
             )}
         </div>
